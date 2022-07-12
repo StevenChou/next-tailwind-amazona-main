2e77ac3b9dc675b3f8f204e444f4142b65ee0ef9
Whalebro <steven.chou.second@gmail.com>
2022-07-12 11:12:06 +0800

    create next app

--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,34 @@
-import axios from 'axios';
-import { useContext } from 'react';
-import { toast } from 'react-toastify';
-import Layout from '../components/Layout';
-import ProductItem from '../components/ProductItem';
-import Product from '../models/Product';
-import db from '../utils/db';
-import { Store } from '../utils/Store';
+import { useContext } from 'react'
+import { toast } from 'react-toastify'
+import axios from 'axios'
+
+import db from '../utils/db'
+import { Store } from '../utils/Store'
+
+import Product from '../models/Product'
+import Layout from '../components/Layout'
+import ProductItem from '../components/ProductItem'
 
 export default function Home({ products }) {
-  const { state, dispatch } = useContext(Store);
-  const { cart } = state;
+  const { state, dispatch } = useContext(Store)
+  const { cart } = state
 
   const addToCartHandler = async (product) => {
-    const existItem = cart.cartItems.find((x) => x.slug === product.slug);
-    const quantity = existItem ? existItem.quantity + 1 : 1;
-    const { data } = await axios.get(`/api/products/${product._id}`);
+    const existItem = cart.cartItems.find((x) => x.slug === product.slug)
+    const quantity = existItem ? existItem.quantity + 1 : 1
+    const { data } = await axios.get(`/api/products/${product._id}`)
 
     if (data.countInStock < quantity) {
-      return toast.error('Sorry. Product is out of stock');
+      return toast.error('Sorry. Product is out of stock')
     }
-    dispatch({ type: 'CART_ADD_ITEM', payload: { ...product, quantity } });
+    dispatch({ type: 'CART_ADD_ITEM', payload: { ...product, quantity } })
 
-    toast.success('Product added to the cart');
-  };
+    toast.success('Product added to the cart')
+  }
 
   return (
-    <Layout title="Home Page">
-      <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4">
+    <Layout title='Home Page'>
+      <div className='grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4'>
         {products.map((product) => (
           <ProductItem
             product={product}
@@ -36,15 +38,15 @@ export default function Home({ products }) {
         ))}
       </div>
     </Layout>
-  );
+  )
 }
 
 export async function getServerSideProps() {
-  await db.connect();
-  const products = await Product.find().lean();
+  await db.connect()
+  const products = await Product.find().lean()
   return {
     props: {
       products: products.map(db.convertDocToObj),
     },
-  };
+  }
 }
