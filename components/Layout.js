021a613358a19b418a443c8ab13fbd920418b956
Whalebro <steven.chou.second@gmail.com>
2022-07-12 11:35:02 +0800

    Create Layout Component

--- components/Layout.js
+++ components/Layout.js
@@ -1,51 +1,54 @@
-import { signOut, useSession } from 'next-auth/react';
-import Head from 'next/head';
-import Link from 'next/link';
-import Cookies from 'js-cookie';
-import React, { useContext, useEffect, useState } from 'react';
-import { ToastContainer } from 'react-toastify';
-import { Menu } from '@headlessui/react';
-import 'react-toastify/dist/ReactToastify.css';
-import { Store } from '../utils/Store';
-import DropdownLink from './DropdownLink';
+import React, { useContext, useEffect, useState } from 'react'
+import { ToastContainer } from 'react-toastify'
+import { signOut, useSession } from 'next-auth/react'
+import Head from 'next/head'
+import Link from 'next/link'
+import Cookies from 'js-cookie'
+import { Menu } from '@headlessui/react'
+
+import 'react-toastify/dist/ReactToastify.css'
+
+import { Store } from '../utils/Store'
+
+import DropdownLink from './DropdownLink'
 
 export default function Layout({ title, children }) {
-  const { status, data: session } = useSession();
+  const { status, data: session } = useSession()
 
-  const { state, dispatch } = useContext(Store);
-  const { cart } = state;
-  const [cartItemsCount, setCartItemsCount] = useState(0);
+  const { state, dispatch } = useContext(Store)
+  const { cart } = state
+  const [cartItemsCount, setCartItemsCount] = useState(0)
   useEffect(() => {
-    setCartItemsCount(cart.cartItems.reduce((a, c) => a + c.quantity, 0));
-  }, [cart.cartItems]);
+    setCartItemsCount(cart.cartItems.reduce((a, c) => a + c.quantity, 0))
+  }, [cart.cartItems])
 
   const logoutClickHandler = () => {
-    Cookies.remove('cart');
-    dispatch({ type: 'CART_RESET' });
-    signOut({ callbackUrl: '/login' });
-  };
+    Cookies.remove('cart')
+    dispatch({ type: 'CART_RESET' })
+    signOut({ callbackUrl: '/login' })
+  }
   return (
     <>
       <Head>
         <title>{title ? title + ' - Amazona' : 'Amazona'}</title>
-        <meta name="description" content="Ecommerce Website" />
-        <link rel="icon" href="/favicon.ico" />
+        <meta name='description' content='Ecommerce Website' />
+        <link rel='icon' href='/favicon.ico' />
       </Head>
 
-      <ToastContainer position="bottom-center" limit={1} />
+      <ToastContainer position='bottom-center' limit={1} />
 
-      <div className="flex min-h-screen flex-col justify-between ">
+      <div className='flex min-h-screen flex-col justify-between '>
         <header>
-          <nav className="flex h-12 items-center px-4 justify-between shadow-md">
-            <Link href="/">
-              <a className="text-lg font-bold">amazona</a>
+          <nav className='flex h-12 items-center px-4 justify-between shadow-md'>
+            <Link href='/'>
+              <a className='text-lg font-bold'>amazona</a>
             </Link>
             <div>
-              <Link href="/cart">
-                <a className="p-2">
+              <Link href='/cart'>
+                <a className='p-2'>
                   Cart
                   {cartItemsCount > 0 && (
-                    <span className="ml-1 rounded-full bg-red-600 px-2 py-1 text-xs font-bold text-white">
+                    <span className='ml-1 rounded-full bg-red-600 px-2 py-1 text-xs font-bold text-white'>
                       {cartItemsCount}
                     </span>
                   )}
@@ -55,20 +58,20 @@ export default function Layout({ title, children }) {
               {status === 'loading' ? (
                 'Loading'
               ) : session?.user ? (
-                <Menu as="div" className="relative inline-block">
-                  <Menu.Button className="text-blue-600">
+                <Menu as='div' className='relative inline-block'>
+                  <Menu.Button className='text-blue-600'>
                     {session.user.name}
                   </Menu.Button>
-                  <Menu.Items className="absolute right-0 w-56 origin-top-right bg-white  shadow-lg ">
+                  <Menu.Items className='absolute right-0 w-56 origin-top-right bg-white  shadow-lg '>
                     <Menu.Item>
-                      <DropdownLink className="dropdown-link" href="/profile">
+                      <DropdownLink className='dropdown-link' href='/profile'>
                         Profile
                       </DropdownLink>
                     </Menu.Item>
                     <Menu.Item>
                       <DropdownLink
-                        className="dropdown-link"
-                        href="/order-history"
+                        className='dropdown-link'
+                        href='/order-history'
                       >
                         Order History
                       </DropdownLink>
@@ -76,8 +79,8 @@ export default function Layout({ title, children }) {
                     {session.user.isAdmin && (
                       <Menu.Item>
                         <DropdownLink
-                          className="dropdown-link"
-                          href="/admin/dashboard"
+                          className='dropdown-link'
+                          href='/admin/dashboard'
                         >
                           Admin Dashboard
                         </DropdownLink>
@@ -85,8 +88,8 @@ export default function Layout({ title, children }) {
                     )}
                     <Menu.Item>
                       <a
-                        className="dropdown-link"
-                        href="#"
+                        className='dropdown-link'
+                        href='#'
                         onClick={logoutClickHandler}
                       >
                         Logout
@@ -95,18 +98,20 @@ export default function Layout({ title, children }) {
                   </Menu.Items>
                 </Menu>
               ) : (
-                <Link href="/login">
-                  <a className="p-2">Login</a>
+                <Link href='/login'>
+                  <a className='p-2'>Login</a>
                 </Link>
               )}
             </div>
           </nav>
         </header>
-        <main className="container m-auto mt-4 px-4">{children}</main>
-        <footer className="flex h-10 justify-center items-center shadow-inner">
+
+        <main className='container m-auto mt-4 px-4'>{children}</main>
+
+        <footer className='flex h-10 justify-center items-center shadow-inner'>
           <p>Copyright © 2022 Amazona</p>
         </footer>
       </div>
     </>
-  );
+  )
 }
