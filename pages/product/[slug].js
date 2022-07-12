bcd8572b3ee51a377be8675b67f9ca544b516673
Whalebro <steven.chou.second@gmail.com>
2022-07-12 13:30:10 +0800

    Details Product

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,54 +1,58 @@
-import axios from 'axios';
-import Image from 'next/image';
-import Link from 'next/link';
-import { useRouter } from 'next/router';
-import React, { useContext } from 'react';
-import { toast } from 'react-toastify';
-import Layout from '../../components/Layout';
-import Product from '../../models/Product';
-import db from '../../utils/db';
-import { Store } from '../../utils/Store';
+import React, { useContext } from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import axios from 'axios'
+import { toast } from 'react-toastify'
+
+import db from '../../utils/db'
+import { Store } from '../../utils/Store'
+
+import Product from '../../models/Product'
+import Layout from '../../components/Layout'
 
 export default function ProductScreen(props) {
-  const { product } = props;
-  const { state, dispatch } = useContext(Store);
-  const router = useRouter();
+  const router = useRouter()
+
+  const { product } = props
+  const { state, dispatch } = useContext(Store)
+
   if (!product) {
-    return <Layout title="Produt Not Found">Produt Not Found</Layout>;
+    return <Layout title='Produt Not Found'>Produt Not Found</Layout>
   }
 
   const addToCartHandler = async () => {
-    const existItem = state.cart.cartItems.find((x) => x.slug === product.slug);
-    const quantity = existItem ? existItem.quantity + 1 : 1;
-    const { data } = await axios.get(`/api/products/${product._id}`);
+    const existItem = state.cart.cartItems.find((x) => x.slug === product.slug)
+    const quantity = existItem ? existItem.quantity + 1 : 1
+    const { data } = await axios.get(`/api/products/${product._id}`)
 
     if (data.countInStock < quantity) {
-      return toast.error('Sorry. Product is out of stock');
+      return toast.error('Sorry. Product is out of stock')
     }
 
-    dispatch({ type: 'CART_ADD_ITEM', payload: { ...product, quantity } });
-    router.push('/cart');
-  };
+    dispatch({ type: 'CART_ADD_ITEM', payload: { ...product, quantity } })
+    router.push('/cart')
+  }
 
   return (
     <Layout title={product.name}>
-      <div className="py-2">
-        <Link href="/">back to products</Link>
+      <div className='py-2'>
+        <Link href='/'>back to products</Link>
       </div>
-      <div className="grid md:grid-cols-4 md:gap-3">
-        <div className="md:col-span-2">
+      <div className='grid md:grid-cols-4 md:gap-3'>
+        <div className='md:col-span-2'>
           <Image
             src={product.image}
             alt={product.name}
             width={640}
             height={640}
-            layout="responsive"
-          ></Image>
+            layout='responsive'
+          />
         </div>
         <div>
           <ul>
             <li>
-              <h1 className="text-lg">{product.name}</h1>
+              <h1 className='text-lg'>{product.name}</h1>
             </li>
             <li>Category: {product.category}</li>
             <li>Brand: {product.brand}</li>
@@ -59,17 +63,17 @@ export default function ProductScreen(props) {
           </ul>
         </div>
         <div>
-          <div className="card p-5">
-            <div className="mb-2 flex justify-between">
+          <div className='card p-5'>
+            <div className='mb-2 flex justify-between'>
               <div>Price</div>
               <div>${product.price}</div>
             </div>
-            <div className="mb-2 flex justify-between">
+            <div className='mb-2 flex justify-between'>
               <div>Status</div>
               <div>{product.countInStock > 0 ? 'In stock' : 'Unavailable'}</div>
             </div>
             <button
-              className="primary-button w-full"
+              className='primary-button w-full'
               onClick={addToCartHandler}
             >
               Add to cart
@@ -78,19 +82,20 @@ export default function ProductScreen(props) {
         </div>
       </div>
     </Layout>
-  );
+  )
 }
 
-export async function getServerSideProps(context) {
-  const { params } = context;
-  const { slug } = params;
+export async function getServerSideProps(ctx) {
+  const { params } = ctx
+  const { slug } = params
 
-  await db.connect();
-  const product = await Product.findOne({ slug }).lean();
-  await db.disconnect();
+  await db.connect()
+  const product = await Product.findOne({ slug }).lean()
+  await db.disconnect()
+
   return {
     props: {
       product: product ? db.convertDocToObj(product) : null,
     },
-  };
+  }
 }
