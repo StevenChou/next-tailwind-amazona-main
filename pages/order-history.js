d9e66c2348c264feadad6961b191cd71289ab560
Whalebro <steven.chou.second@gmail.com>
2022-07-13 15:38:58 +0800

    Create Order History Screen

--- pages/order-history.js
+++ pages/order-history.js
@@ -1,19 +1,21 @@
-import axios from 'axios';
-import Link from 'next/link';
-import React, { useEffect, useReducer } from 'react';
-import Layout from '../components/Layout';
-import { getError } from '../utils/error';
+import React, { useEffect, useReducer } from 'react'
+import Link from 'next/link'
+import axios from 'axios'
+
+import { getError } from '../utils/error'
+
+import Layout from '../components/Layout'
 
 function reducer(state, action) {
   switch (action.type) {
     case 'FETCH_REQUEST':
-      return { ...state, loading: true, error: '' };
+      return { ...state, loading: true, error: '' }
     case 'FETCH_SUCCESS':
-      return { ...state, loading: false, orders: action.payload, error: '' };
+      return { ...state, loading: false, orders: action.payload, error: '' }
     case 'FETCH_FAIL':
-      return { ...state, loading: false, error: action.payload };
+      return { ...state, loading: false, error: action.payload }
     default:
-      return state;
+      return state
   }
 }
 function OrderHistoryScreen() {
@@ -21,57 +23,61 @@ function OrderHistoryScreen() {
     loading: true,
     orders: [],
     error: '',
-  });
+  })
 
   useEffect(() => {
     const fetchOrders = async () => {
       try {
-        dispatch({ type: 'FETCH_REQUEST' });
-        const { data } = await axios.get(`/api/orders/history`);
-        dispatch({ type: 'FETCH_SUCCESS', payload: data });
+        dispatch({ type: 'FETCH_REQUEST' })
+
+        const { data } = await axios.get(`/api/orders/history`)
+
+        dispatch({ type: 'FETCH_SUCCESS', payload: data })
       } catch (err) {
-        dispatch({ type: 'FETCH_FAIL', payload: getError(err) });
+        dispatch({ type: 'FETCH_FAIL', payload: getError(err) })
       }
-    };
-    fetchOrders();
-  }, []);
+    }
+
+    fetchOrders()
+  }, [])
+
   return (
-    <Layout title="Order History">
-      <h1 className="mb-4 text-xl">Order History</h1>
+    <Layout title='Order History'>
+      <h1 className='mb-4 text-xl'>Order History</h1>
       {loading ? (
         <div>Loading...</div>
       ) : error ? (
-        <div className="alert-error">{error}</div>
+        <div className='alert-error'>{error}</div>
       ) : (
-        <div className="overflow-x-auto">
-          <table className="min-w-full">
-            <thead className="border-b">
+        <div className='overflow-x-auto'>
+          <table className='min-w-full'>
+            <thead className='border-b'>
               <tr>
-                <th className="px-5 text-left">ID</th>
-                <th className="p-5 text-left">DATE</th>
-                <th className="p-5 text-left">TOTAL</th>
-                <th className="p-5 text-left">PAID</th>
-                <th className="p-5 text-left">DELIVERED</th>
-                <th className="p-5 text-left">ACTION</th>
+                <th className='px-5 text-left'>ID</th>
+                <th className='p-5 text-left'>DATE</th>
+                <th className='p-5 text-left'>TOTAL</th>
+                <th className='p-5 text-left'>PAID</th>
+                <th className='p-5 text-left'>DELIVERED</th>
+                <th className='p-5 text-left'>ACTION</th>
               </tr>
             </thead>
             <tbody>
               {orders.map((order) => (
-                <tr key={order._id} className="border-b">
-                  <td className=" p-5 ">{order._id.substring(20, 24)}</td>
-                  <td className=" p-5 ">{order.createdAt.substring(0, 10)}</td>
-                  <td className=" p-5 ">${order.totalPrice}</td>
-                  <td className=" p-5 ">
+                <tr key={order._id} className='border-b'>
+                  <td className=' p-5 '>{order._id.substring(20, 24)}</td>
+                  <td className=' p-5 '>{order.createdAt.substring(0, 10)}</td>
+                  <td className=' p-5 '>${order.totalPrice}</td>
+                  <td className=' p-5 '>
                     {order.isPaid
                       ? `${order.paidAt.substring(0, 10)}`
                       : 'not paid'}
                   </td>
-                  <td className=" p-5 ">
+                  <td className=' p-5 '>
                     {order.isDelivered
                       ? `${order.deliveredAt.substring(0, 10)}`
                       : 'not delivered'}
                   </td>
-                  <td className=" p-5 ">
+                  <td className=' p-5 '>
                     <Link href={`/order/${order._id}`} passHref>
                       <a>Details</a>
                     </Link>
@@ -83,8 +89,8 @@ function OrderHistoryScreen() {
         </div>
       )}
     </Layout>
-  );
+  )
 }
 
-OrderHistoryScreen.auth = true;
-export default OrderHistoryScreen;
+OrderHistoryScreen.auth = true
+export default OrderHistoryScreen
