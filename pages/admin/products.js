e4759b093d6d4d799ede41084e17bbd6e1a0693c
Whalebro <steven.chou.second@gmail.com>
2022-07-14 12:04:51 +0800

    Admin Products

--- pages/admin/products.js
+++ pages/admin/products.js
@@ -1,40 +1,42 @@
-import axios from 'axios';
-import Link from 'next/link';
-import { useRouter } from 'next/router';
-import React, { useEffect, useReducer } from 'react';
-import { toast } from 'react-toastify';
-import Layout from '../../components/Layout';
-import { getError } from '../../utils/error';
+import React, { useEffect, useReducer } from 'react'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import { toast } from 'react-toastify'
+import axios from 'axios'
+
+import { getError } from '../../utils/error'
+
+import Layout from '../../components/Layout'
 
 function reducer(state, action) {
   switch (action.type) {
     case 'FETCH_REQUEST':
-      return { ...state, loading: true, error: '' };
+      return { ...state, loading: true, error: '' }
     case 'FETCH_SUCCESS':
-      return { ...state, loading: false, products: action.payload, error: '' };
+      return { ...state, loading: false, products: action.payload, error: '' }
     case 'FETCH_FAIL':
-      return { ...state, loading: false, error: action.payload };
+      return { ...state, loading: false, error: action.payload }
     case 'CREATE_REQUEST':
-      return { ...state, loadingCreate: true };
+      return { ...state, loadingCreate: true }
     case 'CREATE_SUCCESS':
-      return { ...state, loadingCreate: false };
+      return { ...state, loadingCreate: false }
     case 'CREATE_FAIL':
-      return { ...state, loadingCreate: false };
+      return { ...state, loadingCreate: false }
     case 'DELETE_REQUEST':
-      return { ...state, loadingDelete: true };
+      return { ...state, loadingDelete: true }
     case 'DELETE_SUCCESS':
-      return { ...state, loadingDelete: false, successDelete: true };
+      return { ...state, loadingDelete: false, successDelete: true }
     case 'DELETE_FAIL':
-      return { ...state, loadingDelete: false };
+      return { ...state, loadingDelete: false }
     case 'DELETE_RESET':
-      return { ...state, loadingDelete: false, successDelete: false };
+      return { ...state, loadingDelete: false, successDelete: false }
 
     default:
-      state;
+      state
   }
 }
 export default function AdminProdcutsScreen() {
-  const router = useRouter();
+  const router = useRouter()
 
   const [
     { loading, error, products, loadingCreate, successDelete, loadingDelete },
@@ -43,126 +45,136 @@ export default function AdminProdcutsScreen() {
     loading: true,
     products: [],
     error: '',
-  });
+  })
 
-  const createHandler = async () => {
-    if (!window.confirm('Are you sure?')) {
-      return;
-    }
-    try {
-      dispatch({ type: 'CREATE_REQUEST' });
-      const { data } = await axios.post(`/api/admin/products`);
-      dispatch({ type: 'CREATE_SUCCESS' });
-      toast.success('Product created successfully');
-      router.push(`/admin/product/${data.product._id}`);
-    } catch (err) {
-      dispatch({ type: 'CREATE_FAIL' });
-      toast.error(getError(err));
-    }
-  };
   useEffect(() => {
     const fetchData = async () => {
       try {
-        dispatch({ type: 'FETCH_REQUEST' });
-        const { data } = await axios.get(`/api/admin/products`);
-        dispatch({ type: 'FETCH_SUCCESS', payload: data });
+        dispatch({ type: 'FETCH_REQUEST' })
+        const { data } = await axios.get(`/api/admin/products`)
+        dispatch({ type: 'FETCH_SUCCESS', payload: data })
       } catch (err) {
-        dispatch({ type: 'FETCH_FAIL', payload: getError(err) });
+        dispatch({ type: 'FETCH_FAIL', payload: getError(err) })
       }
-    };
+    }
 
     if (successDelete) {
-      dispatch({ type: 'DELETE_RESET' });
+      dispatch({ type: 'DELETE_RESET' })
     } else {
-      fetchData();
+      fetchData()
     }
-  }, [successDelete]);
+  }, [successDelete])
+
+  const createHandler = async () => {
+    if (!window.confirm('Are you sure?')) {
+      return
+    }
+
+    try {
+      dispatch({ type: 'CREATE_REQUEST' })
+
+      const { data } = await axios.post(`/api/admin/products`)
+
+      dispatch({ type: 'CREATE_SUCCESS' })
+
+      toast.success('Product created successfully')
+
+      router.push(`/admin/product/${data.product._id}`)
+    } catch (err) {
+      dispatch({ type: 'CREATE_FAIL' })
+
+      toast.error(getError(err))
+    }
+  }
 
   const deleteHandler = async (productId) => {
     if (!window.confirm('Are you sure?')) {
-      return;
+      return
     }
+
     try {
-      dispatch({ type: 'DELETE_REQUEST' });
-      await axios.delete(`/api/admin/products/${productId}`);
-      dispatch({ type: 'DELETE_SUCCESS' });
-      toast.success('Product deleted successfully');
+      dispatch({ type: 'DELETE_REQUEST' })
+      await axios.delete(`/api/admin/products/${productId}`)
+      dispatch({ type: 'DELETE_SUCCESS' })
+      toast.success('Product deleted successfully')
     } catch (err) {
-      dispatch({ type: 'DELETE_FAIL' });
-      toast.error(getError(err));
+      dispatch({ type: 'DELETE_FAIL' })
+      toast.error(getError(err))
     }
-  };
+  }
+
   return (
-    <Layout title="Admin Products">
-      <div className="grid md:grid-cols-4 md:gap-5">
+    <Layout title='Admin Products'>
+      <div className='grid md:grid-cols-4 md:gap-5'>
         <div>
           <ul>
             <li>
-              <Link href="/admin/dashboard">Dashboard</Link>
+              <Link href='/admin/dashboard'>Dashboard</Link>
             </li>
             <li>
-              <Link href="/admin/orders">Orders</Link>
+              <Link href='/admin/orders'>Orders</Link>
             </li>
             <li>
-              <Link href="/admin/products">
-                <a className="font-bold">Products</a>
+              <Link href='/admin/products'>
+                <a className='font-bold'>Products</a>
               </Link>
             </li>
             <li>
-              <Link href="/admin/users">Users</Link>
+              <Link href='/admin/users'>Users</Link>
             </li>
           </ul>
         </div>
-        <div className="overflow-x-auto md:col-span-3">
-          <div className="flex justify-between">
-            <h1 className="mb-4 text-xl">Products</h1>
+        <div className='overflow-x-auto md:col-span-3'>
+          <div className='flex justify-between'>
+            <h1 className='mb-4 text-xl'>Products</h1>
             {loadingDelete && <div>Deleting item...</div>}
             <button
               disabled={loadingCreate}
               onClick={createHandler}
-              className="primary-button"
+              className='primary-button'
             >
               {loadingCreate ? 'Loading' : 'Create'}
             </button>
           </div>
+
           {loading ? (
             <div>Loading...</div>
           ) : error ? (
-            <div className="alert-error">{error}</div>
+            <div className='alert-error'>{error}</div>
           ) : (
-            <div className="overflow-x-auto">
-              <table className="min-w-full">
-                <thead className="border-b">
+            <div className='overflow-x-auto'>
+              <table className='min-w-full'>
+                <thead className='border-b'>
                   <tr>
-                    <th className="px-5 text-left">ID</th>
-                    <th className="p-5 text-left">NAME</th>
-                    <th className="p-5 text-left">PRICE</th>
-                    <th className="p-5 text-left">CATEGORY</th>
-                    <th className="p-5 text-left">COUNT</th>
-                    <th className="p-5 text-left">RATING</th>
-                    <th className="p-5 text-left">ACTIONS</th>
+                    <th className='px-5 text-left'>ID</th>
+                    <th className='p-5 text-left'>NAME</th>
+                    <th className='p-5 text-left'>PRICE</th>
+                    <th className='p-5 text-left'>CATEGORY</th>
+                    <th className='p-5 text-left'>COUNT</th>
+                    <th className='p-5 text-left'>RATING</th>
+                    <th className='p-5 text-left'>ACTIONS</th>
                   </tr>
                 </thead>
                 <tbody>
                   {products.map((product) => (
-                    <tr key={product._id} className="border-b">
-                      <td className=" p-5 ">{product._id.substring(20, 24)}</td>
-                      <td className=" p-5 ">{product.name}</td>
-                      <td className=" p-5 ">${product.price}</td>
-                      <td className=" p-5 ">{product.category}</td>
-                      <td className=" p-5 ">{product.countInStock}</td>
-                      <td className=" p-5 ">{product.rating}</td>
-                      <td className=" p-5 ">
+                    <tr key={product._id} className='border-b'>
+                      <td className=' p-5 '>{product._id.substring(20, 24)}</td>
+                      <td className=' p-5 '>{product.name}</td>
+                      <td className=' p-5 '>${product.price}</td>
+                      <td className=' p-5 '>{product.category}</td>
+                      <td className=' p-5 '>{product.countInStock}</td>
+                      <td className=' p-5 '>{product.rating}</td>
+                      <td className=' p-5 '>
                         <Link href={`/admin/product/${product._id}`}>
-                          <a type="button" className="default-button">
+                          <a type='button' className='default-button'>
                             Edit
                           </a>
                         </Link>
                         &nbsp;
                         <button
                           onClick={() => deleteHandler(product._id)}
-                          className="default-button"
-                          type="button"
+                          className='default-button'
+                          type='button'
                         >
                           Delete
                         </button>
@@ -176,7 +188,7 @@ export default function AdminProdcutsScreen() {
         </div>
       </div>
     </Layout>
-  );
+  )
 }
 
-AdminProdcutsScreen.auth = { adminOnly: true };
+AdminProdcutsScreen.auth = { adminOnly: true }
