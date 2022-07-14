4aa4f11f90eb8045b20e8b2540aa441bd7191709
Whalebro <steven.chou.second@gmail.com>
2022-07-14 11:06:52 +0800

    Create Admin Orders Screen

--- pages/admin/orders.js
+++ pages/admin/orders.js
@@ -1,19 +1,21 @@
-import axios from 'axios';
-import Link from 'next/link';
-import React, { useEffect, useReducer } from 'react';
-import Layout from '../../components/Layout';
-import { getError } from '../../utils/error';
+import React, { useEffect, useReducer } from 'react'
+import Link from 'next/link'
+import axios from 'axios'
+
+import { getError } from '../../utils/error'
+
+import Layout from '../../components/Layout'
 
 function reducer(state, action) {
   switch (action.type) {
     case 'FETCH_REQUEST':
-      return { ...state, loading: true, error: '' };
+      return { ...state, loading: true, error: '' }
     case 'FETCH_SUCCESS':
-      return { ...state, loading: false, orders: action.payload, error: '' };
+      return { ...state, loading: false, orders: action.payload, error: '' }
     case 'FETCH_FAIL':
-      return { ...state, loading: false, error: action.payload };
+      return { ...state, loading: false, error: action.payload }
     default:
-      state;
+      state
   }
 }
 
@@ -22,88 +24,92 @@ export default function AdminOrderScreen() {
     loading: true,
     orders: [],
     error: '',
-  });
+  })
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        dispatch({ type: 'FETCH_REQUEST' });
-        const { data } = await axios.get(`/api/admin/orders`);
-        dispatch({ type: 'FETCH_SUCCESS', payload: data });
+        dispatch({ type: 'FETCH_REQUEST' })
+
+        const { data } = await axios.get(`/api/admin/orders`)
+
+        dispatch({ type: 'FETCH_SUCCESS', payload: data })
       } catch (err) {
-        dispatch({ type: 'FETCH_FAIL', payload: getError(err) });
+        dispatch({ type: 'FETCH_FAIL', payload: getError(err) })
       }
-    };
-    fetchData();
-  }, []);
+    }
+
+    fetchData()
+  }, [])
 
   return (
-    <Layout title="Admin Dashboard">
-      <div className="grid md:grid-cols-4 md:gap-5">
+    <Layout title='Admin Dashboard'>
+      <div className='grid md:grid-cols-4 md:gap-5'>
         <div>
           <ul>
             <li>
-              <Link href="/admin/dashboard">Dashboard</Link>
+              <Link href='/admin/dashboard'>Dashboard</Link>
             </li>
             <li>
-              <Link href="/admin/orders">
-                <a className="font-bold">Orders</a>
+              <Link href='/admin/orders'>
+                <a className='font-bold'>Orders</a>
               </Link>
             </li>
             <li>
-              <Link href="/admin/products">Products</Link>
+              <Link href='/admin/products'>Products</Link>
             </li>
             <li>
-              <Link href="/admin/users">Users</Link>
+              <Link href='/admin/users'>Users</Link>
             </li>
           </ul>
         </div>
-        <div className="overflow-x-auto md:col-span-3">
-          <h1 className="mb-4 text-xl">Admin Orders</h1>
+        <div className='overflow-x-auto md:col-span-3'>
+          <h1 className='mb-4 text-xl'>Admin Orders</h1>
 
           {loading ? (
             <div>Loading...</div>
           ) : error ? (
-            <div className="alert-error">{error}</div>
+            <div className='alert-error'>{error}</div>
           ) : (
-            <div className="overflow-x-auto">
-              <table className="min-w-full">
-                <thead className="border-b">
+            <div className='overflow-x-auto'>
+              <table className='min-w-full'>
+                <thead className='border-b'>
                   <tr>
-                    <th className="px-5 text-left">ID</th>
-                    <th className="p-5 text-left">USER</th>
-                    <th className="p-5 text-left">DATE</th>
-                    <th className="p-5 text-left">TOTAL</th>
-                    <th className="p-5 text-left">PAID</th>
-                    <th className="p-5 text-left">DELIVERED</th>
-                    <th className="p-5 text-left">ACTION</th>
+                    <th className='px-5 text-left'>ID</th>
+                    <th className='p-5 text-left'>USER</th>
+                    <th className='p-5 text-left'>DATE</th>
+                    <th className='p-5 text-left'>TOTAL</th>
+                    <th className='p-5 text-left'>PAID</th>
+                    <th className='p-5 text-left'>DELIVERED</th>
+                    <th className='p-5 text-left'>ACTION</th>
                   </tr>
                 </thead>
                 <tbody>
                   {orders.map((order) => (
-                    <tr key={order._id} className="border-b">
-                      <td className="p-5">{order._id.substring(20, 24)}</td>
-                      <td className="p-5">
+                    <tr key={order._id} className='border-b'>
+                      <td className='p-5'>{order._id.substring(20, 24)}</td>
+                      <td className='p-5'>
                         {order.user ? order.user.name : 'DELETED USER'}
                       </td>
-                      <td className="p-5">
+                      <td className='p-5'>
                         {order.createdAt.substring(0, 10)}
                       </td>
-                      <td className="p-5">${order.totalPrice}</td>
-                      <td className="p-5">
+                      <td className='p-5'>${order.totalPrice}</td>
+                      <td className='p-5'>
                         {order.isPaid
                           ? `${order.paidAt.substring(0, 10)}`
                           : 'not paid'}
                       </td>
-                      <td className="p-5">
+                      <td className='p-5'>
                         {order.isDelivered
                           ? `${order.deliveredAt.substring(0, 10)}`
                           : 'not delivered'}
                       </td>
-                      <td className="p-5">
+                      <td className='p-5'>
                         <Link href={`/order/${order._id}`} passHref>
                           <a>Details</a>
                         </Link>
+                        {/* <Link href={`/order/${order._id}`}>Details</Link> */}
                       </td>
                     </tr>
                   ))}
@@ -114,7 +120,7 @@ export default function AdminOrderScreen() {
         </div>
       </div>
     </Layout>
-  );
+  )
 }
 
-AdminOrderScreen.auth = { adminOnly: true };
+AdminOrderScreen.auth = { adminOnly: true }
